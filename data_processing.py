f5d233ca2abe040d48692a70c3098c7dead82558
Adam Green <adamgreener10@gmail.com>
2025-09-03 18:33:19 -0400

    updated code documentation

--- data_processing.py
+++ data_processing.py
@@ -14,7 +14,9 @@ class DataBuilder():
 
     def get_dataset_2021(self):
 
-        """Method to download and store 2021 Canadian census data
+        """
+        Method to download and store 2021 Canadian Census data
+
         """
 
         # retrieve file locations and data query string from config file
@@ -46,6 +48,11 @@ class DataBuilder():
 
 
     def get_dataset_2016(self):
+
+        """
+        Method to download and store 2016 Canadian Census data
+
+        """
         
         # retrieve file locations and data query string from config file
         self.config.read('config.cfg')
@@ -77,8 +84,11 @@ class DataBuilder():
 
 
     def combine_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        """Method to implement cleaning and aggregations functions
+
+        """
+        Method to implement cleaning and aggregations functions
         Returns: tuple of dataframes containing final output data
+
         """
         # Complete cleaning process for each vintage
         self._clean_data_2016()
@@ -91,6 +101,11 @@ class DataBuilder():
 
 
     def _clean_data_2021(self):
+
+        """
+        Method to clean 2021 Canadian Census data in preparation for joining with 2016 data
+
+        """
 
         # ingest config
         self.config.read('config.cfg')
@@ -108,9 +123,19 @@ class DataBuilder():
 
     def _clean_data_2016(self):
 
+        """
+        Method to clean 2021 Canadian Census data in preparation for joining with 2016 data
+
+        """
+
+        # ingest config data
         self.config.read('config.cfg')
         output_file = self.config['files']['OUTPUT_FILE_2016']
+
+        # read downloaded 2016 dataset
         data_2016_raw = pd.read_csv(output_file)
+
+        #filter and clean data
         data_2016_raw = data_2016_raw[~data_2016_raw['Dim: Sex (3): Member ID: [1]: Total - Sex'].isin(['x', 'F', '..'])]
         data_2016_raw['DIM: Profile of Census Tracts (2247)'] = data_2016_raw['DIM: Profile of Census Tracts (2247)'].map(lambda x: x.lstrip(), na_action = 'ignore')
         data_2016_raw = data_2016_raw[ast.literal_eval(self.config['columns']['COLUMNS_2016'])][(data_2016_raw['Member ID: Profile of Census Tracts (2247)'].isin(ast.literal_eval(self.config['characteristics']['CHAR_CODES']))) | (data_2016_raw['DIM: Profile of Census Tracts (2247)'].isin(ast.literal_eval(self.config['characteristics']['CHAR_NAMES'])))]
@@ -121,19 +146,40 @@ class DataBuilder():
     
 
     def _generate_long_dataset(self):
+
+        """
+        Method to generate the combined dataset of 2016 and 2021 census data at the granular census tract level
+
+        """
+        # select unique census tracts from 2021 dataset and use them to filter 2016 census tracts
         geos = self.data_2021['ALT_GEO_CODE'].unique()
         filtered_data_2016 = self.data_2016[self.data_2016['ALT_GEO_CODE'].isin(geos)]
+
+        # join datasets
         long_form_data = pd.concat([filtered_data_2016, self.data_2021])
         long_form_data['CHARACTERISTIC_NAME'] = long_form_data['CHARACTERISTIC_NAME'].apply(lambda x: x.split(",")[0] if x.split(",")[0] == 'Population' else x)
+        
+        # arrange demographic variables as columns
         long_form_data = long_form_data.pivot_table(index = ['ALT_GEO_CODE', 'CENSUS_YEAR', 'AGGREGATE_CT'], columns = ['CHARACTERISTIC_NAME'], values = 'C1_COUNT_TOTAL').reset_index()
         return long_form_data
 
 
     def _generate_aggregate_dataset(self):
+
+        """
+        Method to generate the combined dataset of 2016 and 2021 census data at the combined (parent) census tract level
+
+        """
+
+        # select aggregate census tracts and use them to filter 2016 data
         agg_geos = self.data_2021['AGGREGATE_CT'].unique()
         filtered_data_2016 = self.data_2016[self.data_2016['AGGREGATE_CT'].isin(agg_geos)]
+
+        # join 2016 and 2021 data
         long_data = pd.concat([filtered_data_2016, self.data_2021])
         long_data['CHARACTERISTIC_NAME'] = long_data['CHARACTERISTIC_NAME'].apply(lambda x: x.split(",")[0] if x.split(",")[0] == 'Population' else x)
+        
+        # arrange demographic variables in columns and group the variables into their parent census tract
         long_data = long_data.pivot_table(index = ['ALT_GEO_CODE', 'CENSUS_YEAR', 'AGGREGATE_CT'], columns = ['CHARACTERISTIC_NAME'], values = 'C1_COUNT_TOTAL').reset_index()
         agg_data = long_data.drop(columns = 'ALT_GEO_CODE').groupby(['AGGREGATE_CT', 'CENSUS_YEAR']).median().reset_index()
         return agg_data
--- gather_data.py
+++ gather_data.py
@@ -4,18 +4,25 @@ from configparser import ConfigParser
 
 
 def main():
+
     config = ConfigParser()
     config.read('config.cfg')
+
+    # create class instance for gatehering data
     proc = dp.DataBuilder()
     
+    # get 2021 data if it has not yet been retrieved
     if not os.path.exists(config['files']['OUTPUT_FILE_2021']):
         proc.get_dataset_2021()
 
+    # get 2016 data if it has not yet been retrieved
     if not os.path.exists(config['files']['OUTPUT_FILE_2016']):
         proc.get_dataset_2016()
     
+    # combine datasets
     final_data_long, final_data_wide = proc.combine_data()
 
+    # write final datasets to working directory
     final_data_long.to_csv(config['files']['FINAL_DATA_LONG'])
     final_data_wide.to_csv(config['files']['FINAL_DATA_AGG'])
     
